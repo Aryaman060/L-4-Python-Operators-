38dac847540d4c82d80f91bda1d8369950311f94
Aryaman060 <pca1948@gmail.com>
2024-11-07 19:22:14 +0530

    Opretars

---
+++ operations.py
@@ -0,0 +1,11 @@
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 95
+tree_5 = 12
+
+sum = tree_1 + tree_2 + tree_3 + tree_4 + tree_5
+print("The sum of all the 5 Trees is : ", sum)
+
+average = sum/5
+print("The Average of all thr Trees is : ", average)
